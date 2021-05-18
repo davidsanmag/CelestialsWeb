73405425ba1256cbe2a101fa0c92f593f06fb78f
davidsanmag <A01720476@itesm.mx>
2021-05-18 17:14:41 -0500

    backend leaderboard

---
+++ RetoTernium/Model/Scores.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RetoTernium.Model
+{
+    public class Scores
+    {
+        public string username { get; set; }
+        public int score { get; set; }
+    }
+}
--- RetoTernium/Pages/Leaderboard.cshtml.cs
+++ RetoTernium/Pages/Leaderboard.cshtml.cs
@@ -4,13 +4,43 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using RetoTernium.Model;
+using System.Web;
+using System.Net.Http;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Text;
+using System.Net.Http.Headers;
+using System.Data;
 
 namespace RetoTernium.Pages
 {
     public class LeaderboardModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public List<Scores> listScores { get; set; }
+
+        public async Task OnGetAsync(string result)
         {
+
+            string responseContent = "[]";
+            string URL = $"https://chatarrap-api.herokuapp.com/attempts/scoresPast";
+
+            //Buscamos el recurso
+            Uri baseURL = new Uri(URL);
+
+            //Creamos el cliente para que haga nuestra peticion
+            HttpClient client = new HttpClient();
+            string token = HttpContext.Session.GetString("token");
+            client.DefaultRequestHeaders.Add("auth_key", token);
+            HttpResponseMessage response = await client.GetAsync(baseURL.ToString());
+
+            responseContent = await response.Content.ReadAsStringAsync();
+            listScores = JsonConvert.DeserializeObject<List<Scores>>(responseContent);
+
+
         }
     }
 }
